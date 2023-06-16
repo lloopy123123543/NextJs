5631f6dcde8f2d9f617ef5bc1ea6b62ad154df9f
Igor Parfenov <lloopy123123543@gmail.com>
2023-06-16 19:25:36 +0500

    calculations

--- front/app/calculator.js
+++ front/app/calculator.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { Component, useRef } from "react";
+import React, { Component, useRef, useState, useEffect } from "react";
 
 function Calculator() {
   let type = useRef(1);
@@ -7,7 +7,9 @@ function Calculator() {
   let s_stvorka2 = useRef();
   let s_stvorka3 = useRef();
   let s_camera = useRef();
-
+  let height = useRef();
+  let width = useRef();
+  const [summ, setSumm] = useState(0)
 
   function typeCheck(e) {
     let window = document.getElementById("window")
@@ -42,7 +44,6 @@ function Calculator() {
   }
 
   function selectorCheck(e){
-
     console.log("selectorCheck");
     // if(s_stvorka2.current.value > 1){
     //   type.current = 2
@@ -51,13 +52,87 @@ function Calculator() {
     // }
   }
 
+  function SendCalculate(){
+    fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: {
+        stvorka1: s_stvorka1.current,
+        stvorka2: s_stvorka2.current,
+        stvorka3: s_stvorka3.current,
+        height: height.current,
+        weight: width.current,
+        steklopacket: s_camera.current,
 
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
 
+  }
 
+  function calculate(){
 
+    let calculate_stvorka1 = 400;
+    let calculate_stvorka2 = 400;
+    let calculate_stvorka3 = 400;
+    let stvorka2 = document.getElementById("stvorka2");
+    let stvorka3 = document.getElementById("stvorka3");
+    let sizes = (height.current.value + width.current.value)
+
+    const calculateCameraMap = {
+      1: 0.00002,
+      2: 0.00005,
+      3: 0.00008
+    }
+    let calculate_camera = calculateCameraMap[s_camera.current.value]
+
+    setSumm(Math.round((calculate_stvorka1 + sizes) * calculate_camera));
+
+    if(stvorka2.value > 1){
+      setSumm(Math.round((calculate_stvorka1 + calculate_stvorka2 + sizes) * calculate_camera));
+    }
+    if(stvorka3.value > 1){
+      setSumm(Math.round((calculate_stvorka1 + calculate_stvorka2 + calculate_stvorka3 + sizes) * calculate_camera));
+    }
+  }
 
   return (
+    
     <div className="container mx-auto w-full grid grid-cols-2">
+      <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+
+            <div class="p-6 space-y-6">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+
+            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>
       <div className="p-10">
         <img id="window" src="https://www.veka.ru/api-site/calculator/picture?sash_types[0]=3&window_type_id=1"></img>
       </div>
@@ -178,13 +253,13 @@ function Calculator() {
               <div className="block mb-2 text-sm font-medium text-gray-900">
                 Ширина, мм
               </div>
-              <input className="p-2 border border-black w-full"></input>
+              <input type="number" defaultValue='800' ref={width} className="p-2 border border-black w-full"></input>
             </div>
             <div>
               <div className="block mb-2 text-sm font-medium text-gray-900">
                 Высота, мм
               </div>
-              <input className="p-2 border border-black w-full"></input>
+              <input type="number" defaultValue='800' ref={height} className="p-2 border border-black w-full"></input>
             </div>
 
             <div>
@@ -204,7 +279,13 @@ function Calculator() {
             </div>
           </div>
         </div>
+        <div>{summ} рублей</div>
+       <div>
+       <button type="button" onClick={calculate} class="text-white bg-Main hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Посчитать</button>
+       <button type="button" onClick={SendCalculate} class="text-white bg-Main hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Заказать</button>
+       </div>
       </div>
+      
     </div>
   );
 }
