6a4616168b7708fa2f14947ba0a649000b1b1838
Igor Parfenov <lloopy123123543@gmail.com>
2023-06-17 10:08:50 +0500

    calculator adapriv

--- front/app/calculator.js
+++ front/app/calculator.js
@@ -118,7 +118,7 @@ function Calculator() {
 
   return (
     
-    <div className="container mx-auto w-full grid grid-cols-2">
+    <div className="container mx-auto w-full grid grid-cols-1 md:grid-cols-2">
       <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-2xl max-h-full">
 
