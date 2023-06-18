60aae3a25e3e59b5ffc4d10ae7667e23d80b70d4
Igor Parfenov <lloopy123123543@gmail.com>
2023-06-18 10:59:46 +0500

    reviews

--- front/app/reviews.js
+++ front/app/reviews.js
@@ -3,7 +3,30 @@
 import React, { Component, useRef, useState, useEffect } from "react";
 
 
+
+
+
 function Reviews(){
+    const [Review, setReview] = useState([])
+
+useEffect(() => {
+    fetch('http://localhost:8000/api/reviews/show',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(FormData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        setReview(data)
+        console.log(data)
+      })
+      .catch(error => console.log('Failed: ' + error.message));
+  
+
+
+}, [])
     return(
         <div className="w-full">
         <div className="container mx-auto mt-16 grid grid-cols-1">
@@ -11,18 +34,12 @@ function Reviews(){
         <div className="text-5xl font-bold pt-4 text-center ">Отзывы</div>
       </div>
             <div className="container mx-auto p-12 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-20">
-                <div className="bg-white border rounded border-Main p-4 duration-500 shadow-[5px_5px_0px_0px_rgb(288,188,68)] hover:shadow-[-5px_5px_0px_0px_rgb(288,188,68)]">
-                    <div className="text-xl font-bold ">Author</div>
-                    <div className="text-sm text-slate-800">Sgfjdklsfj slkdfjk k jfdk jklskdfjl slksjdf lskjdfdsf</div>
+             {Review.map(rev => <div key={rev.id}>
+                <div className="bg-white h-full flex flex-col text-center items-center justify-center border rounded border-Main p-4 duration-500 shadow-[5px_5px_0px_0px_rgb(288,188,68)] hover:shadow-[-5px_5px_0px_0px_rgb(288,188,68)]">
+                    <div className="text-xl font-bold ">{rev.author}</div>
+                    <div className="text-sm mt-3 text-slate-800">{rev.text}</div>
                 </div>
-                <div className="bg-white border rounded border-Main p-4 duration-500 shadow-[5px_5px_0px_0px_rgb(288,188,68)] hover:shadow-[-5px_5px_0px_0px_rgb(288,188,68)]">
-                    <div className="text-xl font-bold ">Author</div>
-                    <div className="text-sm text-slate-800">Sgfjdklsfj slkdfjk k jfdk jklskdfjl slksjdf lskjdfdsf</div>
-                </div>
-                <div className="bg-white border rounded border-Main p-4 duration-500 shadow-[5px_5px_0px_0px_rgb(288,188,68)] hover:shadow-[-5px_5px_0px_0px_rgb(288,188,68)]">
-                    <div className="text-xl font-bold ">Author</div>
-                    <div className="text-sm text-slate-800">Sgfjdklsfj slkdfjk k jfdk jklskdfjl slksjdf lskjdfdsf</div>
-                </div>
+                    </div>)}
             </div>
         </div>
     )
